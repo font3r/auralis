f96b1e8c21eb69ffcb25bdc16bf39f868d770cc5
fonter <serfin009@gmail.com>
2025-03-16 19:25:24 +0100

    feat: select query parser

---
+++ parser.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+type Command struct{}
+
+type SelectQuery struct {
+	source  SchemeTable[string, string]
+	columns []string
+}
+
+type SchemeTable[T, U string] struct {
+	scheme T
+	name   U
+}
+
+func ParseTokens(tokens []TokenLiteral) (any, error) {
+	valid := hasAnyKeyword(&tokens)
+	if !valid {
+		return Command{}, errors.New("missing keyword")
+	}
+
+	// for now assume that query starts with keyword
+	switch tokens[0].value {
+	case "select":
+		return parseSelect(&tokens)
+	}
+
+	return Command{}, errors.New("missing keyword")
+}
+
+func hasAnyKeyword(tokens *[]TokenLiteral) bool {
+	valid := false
+	for _, v := range *tokens {
+		if v.kind != keyword {
+			continue
+		}
+
+		valid = true
+		break
+	}
+
+	return valid
+}
+
+func parseSelect(tokens *[]TokenLiteral) (SelectQuery, error) {
+	v := *tokens
+	q := SelectQuery{}
+	i := 0
+
+	// select
+	if i >= len(v) || v[i].kind != keyword || v[i].value != "select" {
+		return SelectQuery{}, errors.New("missing select keyword")
+	}
+	i++
+
+	// * or csv columns
+	for ; i < len(v); i++ {
+		if v[i].kind != symbol {
+			break
+		}
+
+		if v[i].value == string(",") {
+			continue
+		}
+
+		q.columns = append(q.columns, v[i].value)
+	}
+
+	if len(q.columns) == 0 {
+		return SelectQuery{}, errors.New("missing columns")
+	}
+	// i is incremented by loop
+
+	// from
+	if i >= len(v) || v[i].kind != keyword || v[i].value != "from" {
+		return SelectQuery{}, errors.New("missing from keyword")
+	}
+	i++
+
+	// source table
+	if i >= len(v) || v[i].kind != symbol {
+		return SelectQuery{}, errors.New("missing source table")
+	}
+
+	s := strings.Split(v[i].value, ".")
+	if len(s) == 1 {
+		q.source = SchemeTable[string, string]{"dbo", s[0]}
+	} else {
+		q.source = SchemeTable[string, string]{s[0], s[1]}
+	}
+
+	return q, nil
+}
---
+++ parser_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestParser(t *testing.T) {
+	testCases := map[string]struct {
+		tokens      []TokenLiteral
+		expectedCmd any
+		expectedErr error
+	}{
+		"query without any keyword": {
+			tokens: []TokenLiteral{
+				{kind: symbol, value: "test"},
+				{kind: symbol, value: "users"},
+			},
+			expectedCmd: Command{},
+			expectedErr: errors.New("missing keyword"),
+		},
+		"query without select keyword": {
+			tokens: []TokenLiteral{
+				{kind: symbol, value: "test"},
+				{kind: keyword, value: "from"},
+				{kind: symbol, value: "users"},
+			},
+			expectedCmd: Command{},
+			expectedErr: errors.New("missing keyword"),
+		},
+		"select without specified columns": {
+			tokens: []TokenLiteral{
+				{kind: keyword, value: "select"},
+				{kind: keyword, value: "from"},
+				{kind: symbol, value: "users"},
+			},
+			expectedCmd: SelectQuery{},
+			expectedErr: errors.New("missing columns"),
+		},
+		"query without from keyword": {
+			tokens: []TokenLiteral{
+				{kind: keyword, value: "select"},
+				{kind: symbol, value: "*"},
+				{kind: symbol, value: "users"},
+			},
+			expectedCmd: SelectQuery{},
+			expectedErr: errors.New("missing from keyword"),
+		},
+		"select without specified source table": {
+			tokens: []TokenLiteral{
+				{kind: keyword, value: "select"},
+				{kind: symbol, value: "*"},
+				{kind: keyword, value: "from"},
+			},
+			expectedCmd: SelectQuery{},
+			expectedErr: errors.New("missing source table"),
+		},
+		"valid select all columns from table": {
+			tokens: []TokenLiteral{
+				{kind: keyword, value: "select"},
+				{kind: symbol, value: "*"},
+				{kind: keyword, value: "from"},
+				{kind: symbol, value: "users"},
+			},
+			expectedCmd: SelectQuery{
+				source:  SchemeTable[string, string]{"dbo", "users"},
+				columns: []string{"*"},
+			},
+		},
+	}
+	for test, tC := range testCases {
+		t.Run(test, func(t *testing.T) {
+			cmd, err := ParseTokens(tC.tokens)
+			// TODO: compare constant errors
+			if err != nil && err.Error() != tC.expectedErr.Error() {
+				t.Errorf("\nexp %+v\ngot %+v", tC.expectedErr, err)
+			} else if !reflect.DeepEqual(cmd, tC.expectedCmd) {
+				t.Errorf("\nexp %+v\ngot %+v", tC.expectedCmd, cmd)
+			}
+		})
+	}
+}
